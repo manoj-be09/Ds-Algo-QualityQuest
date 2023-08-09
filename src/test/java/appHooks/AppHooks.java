337dd9815b720d16f805d59a629fe7927d7d44ee
madhu <madhu@LAPTOP-EGII9MOL.lan>
2023-08-09 10:03:01 -0400

    BASIC OUTLINE

---
+++ src/test/java/appHooks/AppHooks.java
@@ -0,0 +1,5 @@
+package appHooks;
+
+public class AppHooks {
+
+}
---
+++ src/test/java/stepDefinitions/loginPage.java
@@ -0,0 +1,33 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class loginPage {
+
+	@Given("The user is in the Sign in page")
+	public void the_user_is_in_the_sign_in_page() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The user enters a valid {string} and {string}")
+	public void the_user_enters_a_valid_and(String string, String string2) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("Clicks login button")
+	public void clicks_login_button() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("User homepage is displayed")
+	public void user_homepage_is_displayed() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
